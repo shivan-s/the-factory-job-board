c46e8fa85142c1c25e3248b8f95cc37069d2bfbb
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2023-01-28 18:32:23 +1300

    wip

---
+++ src/hooks.server.ts
@@ -0,0 +1,12 @@
+import { SvelteKitAuth } from '@auth/sveltekit';
+import GitHub from '@auth/core/providers/github';
+import Google from '@auth/core/providers/google';
+import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
+import { GOOGLE_ID, GOOGLE_SECRET } from '$env/static/private';
+
+export const handle = SvelteKitAuth({
+  providers: [
+    GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
+    Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })
+  ]
+});
