92cf395cf1950764126c02312ba09bf8c18661fe
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2023-02-10 21:40:15 +1300

    wip

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,18 +1,11 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GitHub from '@auth/core/providers/github';
 import Google from '@auth/core/providers/google';
-import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
-import { GOOGLE_ID, GOOGLE_SECRET } from '$env/static/private';
-import { createContext } from '$lib/trpc/context';
-import { router } from '$lib/trpc/router';
-import type { Handle } from '@sveltejs/kit';
-import { createTRPCHandle } from 'trpc-sveltekit';
+import { GITHUB_ID, GITHUB_SECRET, GOOGLE_ID, GOOGLE_SECRET } from '$env/static/private';
 
-export const authHandle = SvelteKitAuth({
+export const handle = SvelteKitAuth({
   providers: [
     GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
     Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })
   ]
 });
-
-export const handle: Handle = createTRPCHandle({ router, createContext });
